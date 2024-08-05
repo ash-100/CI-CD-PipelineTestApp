904a305eb0f84f4063b90d952a24d90d92ed3400
ash-100 <77534181+ash-100@users.noreply.github.com>
2024-08-05 13:03:41 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,9 +22,18 @@ android {
 
     signingConfigs {
         create("release"){
-
+            
+            val tmpFilePath = System.getProperty("user.home") + "/work/_temp/keystore/"
+            val dir = File(tmpFilePath)
+            val allFilesFromDir = dir.listFiles()
+            
+            if (allFilesFromDir != null && allFilesFromDir.isNotEmpty()) {
+                val keystoreFile = allFilesFromDir.first()
+                val newFilePath = "keystore/keystore.jks"
+                keystoreFile.renameTo(File(newFilePath))
+            }
             storePassword = System.getenv("SIGNING_KEYSTORE_PASSWORD")
-            storeFile = file("keystore.jks")            
+            storeFile = file("keystore/keystore.jks")            
             keyAlias = System.getenv("SIGNING_ALIAS")
             keyPassword = System.getenv("SIGNING_ALIAS_PASSWORD")
         }
